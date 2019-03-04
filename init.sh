91f04cf49b7402b89d8996ad2de67413070f2798
Jebaitedneko <kannanpulimoottil@gmail.com>
2019-03-05 04:40:39 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cp ~/src/main/java/config.conf ~/target && java -cp ~/target/storasbot-0.0.1-SNAPSHOT.jar lt.ekgame.storasbot.StorasDiscord config.conf
+cp ~/src/main/resources/application.conf ~/target && java -cp ~/target/storasbot-0.0.1-SNAPSHOT.jar lt.ekgame.storasbot.StorasDiscord application.conf
