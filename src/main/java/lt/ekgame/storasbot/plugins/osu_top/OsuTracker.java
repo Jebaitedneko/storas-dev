4a35ee8698c6b0b6ce5c18c6e89262f541b99cb7
Jebaitedneko <kannanpulimoottil@gmail.com>
2019-03-05 11:19:19 +0530

    Update OsuTracker.java

--- src/main/java/lt/ekgame/storasbot/plugins/osu_top/OsuTracker.java
+++ src/main/java/lt/ekgame/storasbot/plugins/osu_top/OsuTracker.java
@@ -107,7 +107,7 @@ public class OsuTracker extends Thread implements EventListener {
 				
 				// Add individual trackers (new or append to country tracker) used to be tracker.getidentifier()
 				for (TrackedPlayer tracker : playerTrackers) {
-					OsuUpdatablePlayer updatable = updatablePlayers.get(player.getIdentifier());
+					OsuUpdatablePlayer updatable = updatablePlayers.get(tracker.getIdentifier());
 					updatable.addScoreHandler(tracker);
 				}
 				
